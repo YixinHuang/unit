dbd71e1d4aac8e814fe035d12c257a392950f1dd
root <root@iZ25v38i0ggZ.(none)>
2015-05-04 20:37:43 +0800

    support linux

--- log/log.c
+++ log/log.c
@@ -7,52 +7,53 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
+static u_char *LOG_NAME="/bqmp/hyx/nginx-1.4.4/src/unit/bin";
 
-#define LOG_NAME	"/bqmp/hyx/nginx-1.4.4/src/unit/bin"
-
-ngx_log_t          *log;
+ngx_log_t          *mylog;
 
 int unit_create_log()
 {
-    log = ngx_log_init(LOG_NAME);
-    if (log == NULL) {
+    mylog = ngx_log_init(LOG_NAME);
+    if (mylog == NULL) {
     		printf("ngx_log_init error.\n");
-        return 1;
+        return NGX_ERROR;
     }
-    //log->log_level = 0x7fffffff;//NGX_LOG_DEBUG_ALL;
-    log->log_level = NGX_LOG_DEBUG;
+    /* mylog->log_level = 0x7fffffff;//NGX_LOG_DEBUG_ALL; */
+    mylog->log_level = NGX_LOG_DEBUG;
+    
+    return NGX_OK;
 }
 
 
 int
 main(int argc, char *const *argv)
 {
-		printf("--------Start log Test---------\n");
+		printf("--------Start mylog Test---------\n");
 		int ret;
 		
-		ret = unit_create_log();
+		unit_create_log();
 		
-		if(log){
+		if(mylog){
 
-        ngx_log_error(NGX_LOG_EMERG, log, 0, "ngx_log_error""This is the NGX_LOG_EMERG level. ");				
-        ngx_log_error(NGX_LOG_ALERT, log, 0, "ngx_log_error""This is the NGX_LOG_ALERT level. ");				
-        ngx_log_error(NGX_LOG_CRIT, log, 0, "ngx_log_error""This is the NGX_LOG_CRIT level. ");				                        
-        ngx_log_error(NGX_LOG_ERR, log, 0, "ngx_log_error""This is the NGX_LOG_ERR level. ");				
-        ngx_log_error(NGX_LOG_WARN, log, 0, "ngx_log_error""This is the NGX_LOG_WARN level. ");				
-        ngx_log_error(NGX_LOG_NOTICE, log, 0, "ngx_log_error""This is the NGX_LOG_NOTICE level. ");				
-        ngx_log_error(NGX_LOG_INFO, log, 0, "ngx_log_error""This is the NGX_LOG_INFO level. ");	
-        ngx_log_error(NGX_LOG_DEBUG, log, 0, "ngx_log_error""This is the NGX_LOG_DEBUG level. ");	 
+        ngx_log_error(NGX_LOG_EMERG, mylog, 0, "ngx_log_error""This is the NGX_LOG_EMERG level. ");				
+        ngx_log_error(NGX_LOG_ALERT, mylog, 0, "ngx_log_error""This is the NGX_LOG_ALERT level. ");				
+        ngx_log_error(NGX_LOG_CRIT, mylog, 0, "ngx_log_error""This is the NGX_LOG_CRIT level. ");				                        
+        ngx_log_error(NGX_LOG_ERR, mylog, 0, "ngx_log_error""This is the NGX_LOG_ERR level. ");				
+        ngx_log_error(NGX_LOG_WARN, mylog, 0, "ngx_log_error""This is the NGX_LOG_WARN level. ");				
+        ngx_log_error(NGX_LOG_NOTICE, mylog, 0, "ngx_log_error""This is the NGX_LOG_NOTICE level. ");				
+        ngx_log_error(NGX_LOG_INFO, mylog, 0, "ngx_log_error""This is the NGX_LOG_INFO level. ");	
+        ngx_log_error(NGX_LOG_DEBUG, mylog, 0, "ngx_log_error""This is the NGX_LOG_DEBUG level. ");	 
        
-		    log->log_level = NGX_LOG_DEBUG_HTTP;
-        ngx_log_debug(NGX_LOG_DEBUG_HTTP, log, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_HTTP level. ");
-        ngx_log_debug(NGX_LOG_DEBUG_CORE, log, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_CORE level. ");        
+		    mylog->log_level = NGX_LOG_DEBUG_HTTP;
+        ngx_log_debug(NGX_LOG_DEBUG_HTTP, mylog, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_HTTP level. ");
+        ngx_log_debug(NGX_LOG_DEBUG_CORE, mylog, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_CORE level. ");        
 
-		    log->log_level = NGX_LOG_DEBUG_CORE;
-        ngx_log_debug(NGX_LOG_DEBUG_HTTP, log, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_HTTP level. ");
-        ngx_log_debug(NGX_LOG_DEBUG_CORE, log, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_CORE level. ");           
+		    mylog->log_level = NGX_LOG_DEBUG_CORE;
+        ngx_log_debug(NGX_LOG_DEBUG_HTTP, mylog, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_HTTP level. ");
+        ngx_log_debug(NGX_LOG_DEBUG_CORE, mylog, 0, "ngx_log_debug""This is the NGX_LOG_DEBUG_CORE level. ");           
        					                      
 			}			
-		printf("--------End log Test---------\n");		
+		printf("--------End mylog Test---------\n");		
 		return ret;
 	
 }
